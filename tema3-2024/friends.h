0121e1235bbe9d0b75474432e7c7ecb01cf0087e
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-05-15 02:50:12 +0300

    new functions for add, remove, distance friends

---
+++ tema3-2024/friends.h
@@ -0,0 +1,22 @@
+#ifndef FRIENDS_H
+#define FRIENDS_H
+
+#define MAX_COMMAND_LEN 100
+#define MAX_PEOPLE 550
+
+#include "structuri.h"
+
+/**
+ * Function that handles the calling of every command from task 1
+ *
+ * Please add any necessary parameters to the functions
+*/
+void handle_input_friends(char *input);
+
+void add_friend(list_graph_t *graph, char *name1, char *name2);
+
+void remove_friend(list_graph_t *graph, char *name1, char *name2);
+
+void distance_friend(list_graph_t *graph, char *name1, char *name2);
+
+#endif // FRIENDS_H
