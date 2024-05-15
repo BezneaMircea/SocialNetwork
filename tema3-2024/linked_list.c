8600e0907594bd3c802d92238894e8aa69f2f5a4
Stefan Ghenescu <stefan.ghenescu2005@gmail.com>
2024-05-15 19:21:29 +0300

    coding style repair

--- tema3-2024/linked_list.c
+++ tema3-2024/linked_list.c
@@ -8,10 +8,10 @@
 
 linked_list_t *ll_create(unsigned int data_size)
 {
-	linked_list_t* ll;
+	linked_list_t *ll;
 
 	ll = malloc(sizeof(*ll));
-    DIE(!ll, "Eroare la alocare\n");
+	DIE(!ll, "Eroare la alocare\n");
 
 	ll->head = NULL;
 	ll->data_size = data_size;
@@ -27,19 +27,17 @@ linked_list_t *ll_create(unsigned int data_size)
  * pozitia n=0). Daca n >= nr_noduri, noul nod se adauga la finalul listei. Daca
  * n < 0, eroare.
  */
-void ll_add_nth_node(linked_list_t* list, unsigned int n, const void* new_data)
+void ll_add_nth_node(linked_list_t *list, unsigned int n, const void *new_data)
 {
 	ll_node_t *prev, *curr;
-	ll_node_t* new_node;
+	ll_node_t *new_node;
 
-	if (!list) {
+	if (!list)
 		return;
-	}
 
 	/* n >= list->size inseamna adaugarea unui nou nod la finalul listei. */
-	if (n > list->size) {
+	if (n > list->size)
 		n = list->size;
-	}
 
 	curr = list->head;
 	prev = NULL;
@@ -50,15 +48,15 @@ void ll_add_nth_node(linked_list_t* list, unsigned int n, const void* new_data)
 	}
 
 	new_node = malloc(sizeof(*new_node));
-    DIE(!new_node, "Eroare la alocare\n");
+	DIE(!new_node, "Eroare la alocare\n");
 
 	new_node->data = malloc(list->data_size);
-    DIE(!new_node->data, "Erorare la alocare\n");
+	DIE(!new_node->data, "Erorare la alocare\n");
 
 	memcpy(new_node->data, new_data, list->data_size);
 
 	new_node->next = curr;
-	if (prev == NULL) {
+	if (!prev) {
 		/* Adica n == 0. */
 		list->head = new_node;
 	} else {
@@ -76,18 +74,16 @@ void ll_add_nth_node(linked_list_t* list, unsigned int n, const void* new_data)
  * nod proaspat eliminat din lista. Este responsabilitatea apelantului sa
  * elibereze memoria acestui nod.
  */
-ll_node_t *ll_remove_nth_node(linked_list_t* list, unsigned int n)
+ll_node_t *ll_remove_nth_node(linked_list_t *list, unsigned int n)
 {
 	ll_node_t *prev, *curr;
 
-	if (!list || !list->head) {
+	if (!list || !list->head)
 		return NULL;
-	}
 
 	/* n >= list->size - 1 inseamna eliminarea nodului de la finalul listei. */
-	if (n > list->size - 1) {
+	if (n > list->size - 1)
 		n = list->size - 1;
-	}
 
 	curr = list->head;
 	prev = NULL;
@@ -97,7 +93,7 @@ ll_node_t *ll_remove_nth_node(linked_list_t* list, unsigned int n)
 		--n;
 	}
 
-	if (prev == NULL) {
+	if (!prev) {
 		/* Adica n == 0. */
 		list->head = curr->next;
 	} else {
@@ -113,11 +109,9 @@ ll_node_t *ll_remove_nth_node(linked_list_t* list, unsigned int n)
  * Functia intoarce numarul de noduri din lista al carei pointer este trimis ca
  * parametru.
  */
-unsigned int ll_get_size(linked_list_t* list)
-{
-	 if (!list) {
+unsigned int ll_get_size(linked_list_t *list) {
+	if (!list)
 		return -1;
-	}
 
 	return list->size;
 }
@@ -125,14 +119,14 @@ unsigned int ll_get_size(linked_list_t* list)
 /*
  * Functia primeste ca parametru o lista si imi afiseaza continutul acesteia.
 */
-void ll_print_int(linked_list_t* list) {
+void ll_print_int(linked_list_t *list) {
 	if (!list)
 		return;
 
-	ll_node_t* curr = list->head;
+	ll_node_t *curr = list->head;
 
 	while (curr) {
-		printf("%d ", *(int*)curr->data);
+		printf("%d ", *(int *)curr->data);
 		curr = curr->next;
 	}
 	printf("\n");
@@ -144,20 +138,19 @@ void ll_print_int(linked_list_t* list) {
  * NULL valoarea pointerului la care pointeaza argumentul (argumentul este un
  * pointer la un pointer).
  */
-void ll_free(linked_list_t** pp_list)
+void ll_free(linked_list_t **pp_list)
 {
-	ll_node_t* currNode;
+	ll_node_t *curr_node;
 
-	if (!pp_list || !*pp_list) {
+	if (!pp_list || !*pp_list)
 		return;
-	}
 
 	while (ll_get_size(*pp_list) > 0) {
-		currNode = ll_remove_nth_node(*pp_list, 0);
-		free(currNode->data);
-		currNode->data = NULL;
-		free(currNode);
-		currNode = NULL;
+		curr_node = ll_remove_nth_node(*pp_list, 0);
+		free(curr_node->data);
+		curr_node->data = NULL;
+		free(curr_node);
+		curr_node = NULL;
 	}
 
 	free(*pp_list);
